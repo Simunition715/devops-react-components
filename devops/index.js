d5662d1b3a087122d1c3fd26f9f64b345790fa04
William Blackwell <wbprogramming@icloud.com>
2024-02-01 12:30:10 -0500

    fix for publishing

---
+++ devops/index.js
@@ -0,0 +1,9 @@
+// index.js
+
+const createReactComponent = (componentName) => {
+    console.log(`Creating React component: ${componentName}`);
+    // Logic to create React component files goes here
+  };
+  
+  module.exports = createReactComponent;
+  
--- devops/scripts/create-component.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-exec('node_modules\\.bin\\devops\\scripts\\create-component.sh', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
-  console.log(stdout);
-});
